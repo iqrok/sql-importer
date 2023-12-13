9a22b347daf87985e7ef02ac1c94dc934f139cbf
rusminto <rusminto900@gmail.com>
2023-12-13 21:37:00 +0700

    feat: add test for partial import

--- test/module/all.test.js
+++ test/module/all.test.js
@@ -1,63 +1,16 @@
 const sqlFileImporter = require(__basedir);
-const mysql = new (require(`${__basedir}/lib/mariadb.class.js`))(__config.db);
 const importer = sqlFileImporter.init(__config.db);
-
-
-
-
-async function checkRoutine(){
-    return mysql.query(`
-        SELECT 
-            routine_name, 
-            SUM(IF(routine_type = "FUNCTION", 0, 1)) as isFunction,
-            SUM(IF(routine_type = "PROCEDURE", 0, 1)) as isProcedure
-        FROM information_schema.routines 
-        WHERE routine_schema = ?
-        `,[__config.db.database])
-        .then(res => {
-
-            if(res[0].isFunction <= 0){
-                return false
-            }
-
-            if(res[0].isProcedure <= 0){
-                return false
-            }
-
-            return true
-        })
-        .catch(err => false)
-}
-
-async function checkTable(){
-    return mysql.query(`
-    SELECT table_name FROM information_schema.tables
-    WHERE table_schema = ?
-    `,[__config.db.database])
-    .then(res => {
-        return res[0].table_name == "d_access_proposal" ? true: false
-    })
-    .catch(err => false)
-}
-
-async function checkTableHasContent(){
-    return mysql.query(`
-        SELECT COUNT(1) as total FROM d_access_proposal
-        `)
-        .then(res => {
-            return res[0].total > 0 ? true: false
-        })
-        .catch(err => false)
-}
-
+const gen = require("./general")
 
 
 
 describe("From PMA's export", () => {
 
+
     test("Delete all tables and routine", async () => {
         await importer.emptyDatabase(false);
     })
+
 
     test("Import db structure and data", async () => {
 
@@ -70,24 +23,26 @@ describe("From PMA's export", () => {
 
 
         // check if routines success imported
-        const isRoutineExist = await checkRoutine()
+        const isRoutineExist = await gen.checkRoutine()
         expect(isRoutineExist).toBe(true)
 
 
         // check if table success imported
-        const isTableExist = await checkTable()
+        const isTableExist = await gen.checkTable()
         expect(isTableExist).toBe(true)
 
 
         // check if table has content
-        const isTableHasContent = await checkTableHasContent()
+        const isTableHasContent = await gen.checkTableHasContent()
         expect(isTableHasContent).toBe(true)
 
     })
 
+
     test("Delete all tables and routine", async () => {
         await importer.emptyDatabase(false);
     })
+
 
     test("Import db structure only", async () => {
 
@@ -102,24 +57,26 @@ describe("From PMA's export", () => {
 
 
         // check if routines success imported
-        const isRoutineExist = await checkRoutine()
+        const isRoutineExist = await gen.checkRoutine()
         expect(isRoutineExist).toBe(true)
 
 
         // check if table success imported
-        const isTableExist = await checkTable()
+        const isTableExist = await gen.checkTable()
         expect(isTableExist).toBe(true)
 
 
         // check if table has no content
-        const isTableHasContent = await checkTableHasContent()
+        const isTableHasContent = await gen.checkTableHasContent()
         expect(isTableHasContent).toBe(false)
 
     })
 
+
     test("Delete all tables and routine", async () => {
         await importer.emptyDatabase(false);
     })
+
 
     test("Import db without dropping all tables and routines first", async () => {
 
@@ -134,27 +91,111 @@ describe("From PMA's export", () => {
 
 
         // check if routines success imported
-        const isRoutineExist = await checkRoutine()
+        const isRoutineExist = await gen.checkRoutine()
         expect(isRoutineExist).toBe(true)
 
 
         // check if table success imported
-        const isTableExist = await checkTable()
+        const isTableExist = await gen.checkTable()
         expect(isTableExist).toBe(true)
 
 
         // check if table has no content
-        const isTableHasContent = await checkTableHasContent()
+        const isTableHasContent = await gen.checkTableHasContent()
         expect(isTableHasContent).toBe(true)
 
     })
 
-    test("Import partially (Update Column)", async () => {
 
+    test("Import partially (Add Empty Column)", async () => {
+        // import db from sql file
+        const filepath = __basedir + '/sample/from_pma_add_column.sql';
+
+        const proc = await importer.init(__config.db)
+        .read(filepath)
+        .importFile();
+
+
+        // check if routines success imported
+        const isRoutineExist = await gen.checkRoutine()
+        expect(isRoutineExist).toBe(true)
+
+
+        // check if table success imported
+        const isTableExist = await gen.checkTable()
+        expect(isTableExist).toBe(true)
+
+
+        // check if table has no content
+        const isTableHasContent = await gen.checkTableHasContent()
+        expect(isTableHasContent).toBe(true)
+
+
+        // check if table has column "labCode" and has content "LT" 
+        // meaning only new update that implemented
+        const isTableHasLabCodeLT = await gen.checkTableHasLabCodeLT()
+        expect(isTableHasLabCodeLT).toBe(true)
+
+
+        // check new update
+        // check if table has column "itemCode"
+        const isTableHasItemCode = await gen.checkTableHasItemCode()
+        expect(isTableHasItemCode).toBe(true)
+
+        // check if table has column "itemCode" and has content
+        const isTableHasItemCodeContent = await gen.checkTableHasItemCodeContent()
+        expect(isTableHasItemCodeContent).toBe(false)
     })
 
-    test("Import partially (Update Table)", async () => {
 
+    test("Import partially (Add Empty Table)", async () => {
+        // import db from sql file
+        const filepath = __basedir + '/sample/from_pma_add_table.sql';
+
+        const proc = await importer.init(__config.db)
+        .read(filepath)
+        .importFile();
+
+
+        // check if routines success imported
+        const isRoutineExist = await gen.checkRoutine()
+        expect(isRoutineExist).toBe(true)
+
+
+        // check if table success imported
+        const isTableExist = await gen.checkTable()
+        expect(isTableExist).toBe(true)
+
+
+        // check if table has no content
+        const isTableHasContent = await gen.checkTableHasContent()
+        expect(isTableHasContent).toBe(true)
+
+
+        // check if table has column "labCode" and has content "LT" 
+        // meaning only new update that implemented
+        const isTableHasLabCodeLT = await gen.checkTableHasLabCodeLT()
+        expect(isTableHasLabCodeLT).toBe(true)
+
+
+        // check last update
+        // check if table has column "itemCode"
+        const isTableHasItemCode = await gen.checkTableHasItemCode()
+        expect(isTableHasItemCode).toBe(true)
+
+        // check if table has column "itemCode" and has content
+        const isTableHasItemCodeContent = await gen.checkTableHasItemCodeContent()
+        expect(isTableHasItemCodeContent).toBe(false)
+
+
+        // check new update
+        // check if new table exist
+        const isTableItem = await gen.checkTableItem()
+        expect(isTableItem).toBe(true)
+
+        // check if new table exist and has content
+        const isTableItemContent = await gen.checkTableItemHasContent()
+        expect(isTableItemContent).toBe(false)
     })
 
 })
@@ -162,5 +203,5 @@ describe("From PMA's export", () => {
 
 // close connection
 afterAll(async () => {
-    await mysql.end();
+    gen.close()
 });
